47b35f37f8e6a0890bd342621eb675e45c03f579
Bella Amandine <bellamandy45@gmail.com>
2021-01-24 17:51:55 +0200

    Add Profile model

--- awardapp/models.py
+++ awardapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_picture = models.ImageField(upload_to = 'pictures/', blank = True)
+    bio = models.TextField(blank=True)
+    contact = models.CharField(max_length = 255)
+
