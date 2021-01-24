8a6c4d1dbe8d4624c9fbb68f54613c262e539b3d
Bella Amandine <bellamandy45@gmail.com>
2021-01-24 20:04:28 +0200

    Add Rating model

--- awardapp/models.py
+++ awardapp/models.py
@@ -7,7 +7,16 @@ class Profile(models.Model):
     contact = models.CharField(max_length = 255)
 
 class Project(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='projects')
     project_title = models.CharField(max_length = 255)
     project_description = models.TextField()
     project_image = models.ImageField(upload_to = 'projects/')
     project_link = models.CharField(max_length = 255)
+
+class Rating(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='ratings')
+    comment = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='ratings')
+    design_rate = models.IntegerField(default= 0)
+    usability_rate = models.IntegerField(default= 0)
+    content_rate = models.IntegerField(default= 0)
