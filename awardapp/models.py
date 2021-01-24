9254a489f0272c3d2b176917d3743a1af996d1b9
Bella Amandine <bellamandy45@gmail.com>
2021-01-24 22:59:32 +0200

    Add profile method and test profile methods

--- awardapp/models.py
+++ awardapp/models.py
@@ -1,10 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = 'profile', default = 0)
     profile_picture = models.ImageField(upload_to = 'profiles/', blank = True)
     bio = models.TextField(blank=True)
     contact = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def create_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_all_profiles(cls):
+        return cls.objects.all()
 
 class Project(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='projects', default = 0)
--- awardapp/tests.py
+++ awardapp/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile, Project, Rating
 
 # Create your tests here.
+class ProfileTestCase(TestCase):
+
+    def setUp(self):
+        self.new_user = User(username = "amandine")
+        self.new_user.save()
+
+        self.new_profile = Profile(user = self.new_user, profile_picture = 'default.jpg', bio = "bla bla blaaa", contact = "0782927483")
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile, Profile))
+
+    def test_create_profile_method(self):
+        self.new_profile.create_profile()
+        profiles = Profile.get_all_profiles()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_profile_method(self):
+        self.new_profile.create_profile()
+        self.new_profile.delete_profile()
+        profiles = Profile.get_all_profiles()
+        self.assertTrue(len(profiles) == 0)
+
