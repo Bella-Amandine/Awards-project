9046a05052665535e8bba6f780e95dd1deaf4b26
Bella Amandine <bellamandy45@gmail.com>
2021-01-24 19:38:25 +0200

    Add Project model

--- awardapp/models.py
+++ awardapp/models.py
@@ -5,3 +5,9 @@ class Profile(models.Model):
     profile_picture = models.ImageField(upload_to = 'profiles/', blank = True)
     bio = models.TextField(blank=True)
     contact = models.CharField(max_length = 255)
+
+class Project(models.Model):
+    project_title = models.CharField(max_length = 255)
+    project_description = models.TextField()
+    project_image = models.ImageField(upload_to = 'projects/')
+    project_link = models.CharField(max_length = 255)
