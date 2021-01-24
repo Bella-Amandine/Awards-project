9ac8b66e4ae53808c541491cf7e5c4208ff36016
Bella Amandine <bellamandy45@gmail.com>
2021-01-24 19:37:52 +0200

    Migrate profile model

--- award_project/settings.py
+++ award_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'awardapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- awardapp/models.py
+++ awardapp/models.py
@@ -5,4 +5,3 @@ class Profile(models.Model):
     profile_picture = models.ImageField(upload_to = 'profiles/', blank = True)
     bio = models.TextField(blank=True)
     contact = models.CharField(max_length = 255)
-
