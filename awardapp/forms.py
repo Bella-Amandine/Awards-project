2eb668d06fd7139e571af81872612dd943ae8cbd
Bella Amandine <bellamandy45@gmail.com>
2021-01-30 19:17:59 +0200

    Add project form in forms

--- awardapp/forms.py
+++ awardapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Project, Rating
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=40)
@@ -8,3 +9,8 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['owner']
